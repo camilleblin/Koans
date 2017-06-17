bd3f13f091ddb4c2499c15cebce8143a164aecf7
camille blin <camilleblin@gmail.com>
2017-06-17 19:07:07 +0200

    task 4 + 5

--- lesson2/task4/src/Task.kt
+++ lesson2/task4/src/Task.kt
@@ -1,21 +1,20 @@
 class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
-    override fun iterator(): Iterator<MyDate> {
-        return DateRangeIterator(this)
-    }
+    override fun iterator(): Iterator<MyDate> = DateRangeIterator(this)
+}
 
-    class DateRangeIterator(var dateRange: DateRange) : Iterator<MyDate>{
-        var currentDate = dateRange.start
-        override fun hasNext(): Boolean {
-            return dateRange.contains(currentDate.nextDay())
-        }
+class DateRangeIterator(var dateRange: DateRange) : Iterator<MyDate>{
+    var currentDate = dateRange.start
+    override fun hasNext(): Boolean = currentDate <= dateRange.end
 
-        override fun next(): MyDate {
-            val result = currentDate
-            currentDate = currentDate.nextDay()
-            return result
-        }
+
+    override fun next(): MyDate {
+        val result = currentDate
+        currentDate = currentDate.nextDay()
+        return result
     }
 }
+
+
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- lesson2/task5/src/Task.kt
+++ lesson2/task5/src/Task.kt
@@ -1,15 +1,19 @@
 import TimeInterval.*
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class TimeIntervalRepetition(val timeInterval: TimeInterval, val repetition : Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = this.addTimeIntervals(timeInterval, 1)
+operator fun TimeInterval.times(numberOfRepetition : Int) : TimeIntervalRepetition = TimeIntervalRepetition(this, numberOfRepetition)
+operator fun MyDate.plus(repetition: TimeIntervalRepetition) : MyDate = this.addTimeIntervals(repetition.timeInterval, repetition.repetition)
+
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
