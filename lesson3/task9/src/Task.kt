3724979edbd97e652e53c83d5e1be16dab961f39
camille blin <camilleblin@gmail.com>
2017-06-18 13:31:45 +0200

    to lesson 3

--- lesson3/task9/src/Task.kt
+++ lesson3/task9/src/Task.kt
@@ -1,2 +1,9 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.partition { hasMoreOrderDelivered(it.orders) }.second.toSet()
+
+fun hasMoreOrderDelivered(orders : List<Order>) : Boolean {
+    val (delivered, undelivered) = orders.partition { it.isDelivered }
+    return delivered.size >= undelivered.size
+}
