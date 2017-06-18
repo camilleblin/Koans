9d8f7e0b06f7eed5d07b131681ae77331d429000
camille blin <camilleblin@gmail.com>
2017-06-18 22:04:56 +0200

    fold

--- lesson3/task10/src/Task.kt
+++ lesson3/task10/src/Task.kt
@@ -1,4 +1,7 @@
 // Return the set of products that were ordered by every customer
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
+    return customers.fold(Set<Product>(), {
+        acc, customer -> acc.addAll(customer.orders.flatMap { it.products })
+        acc
+    }).toSet()
 }
--- lesson3/task9/src/Task.kt
+++ lesson3/task9/src/Task.kt
@@ -1,9 +1,7 @@
 import com.sun.org.apache.xpath.internal.operations.Bool
 
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.partition { hasMoreOrderDelivered(it.orders) }.second.toSet()
-
-fun hasMoreOrderDelivered(orders : List<Order>) : Boolean {
-    val (delivered, undelivered) = orders.partition { it.isDelivered }
-    return delivered.size >= undelivered.size
-}
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.filter {
+    val (delivered, undelivered) = it.orders.partition { it.isDelivered }
+    undelivered.size > delivered.size
+}.toSet()
